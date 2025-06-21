5420034dcf1dd56c727a8a008433edc93e38e5d7
S902248 <143501187+S902248@users.noreply.github.com>
2025-06-21 15:30:18 +0530

    Update App.jsx

--- portfolio-app/src/App.jsx
+++ portfolio-app/src/App.jsx
@@ -54,7 +54,8 @@ function App() {
     e.preventDefault();
     setStatus('Sending...');
     try {
-      const res = await fetch('http://localhost:5000/api/contact', {
+      // const res = await fetch('http://localhost:5000/api/contact', {
+         const res = await fetch('https://poerfolio.onrender.com/api/contact', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
